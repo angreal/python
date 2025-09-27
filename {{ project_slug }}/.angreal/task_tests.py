d96202f533abe957f785400eb1143bf900048084
Dylan Storey <dstorey@gmail.com>
2025-09-27 05:36:06 -0400

    updated tests maybe

--- {{ project_slug }}/.angreal/task_tests.py
+++ {{ project_slug }}/.angreal/task_tests.py
@@ -18,8 +18,18 @@ def unit_tests(open=False):
     output_file = os.path.realpath(os.path.join(cwd,'htmlcov','index.html'))
     
     with VirtualEnv(path=venv_path, now=True) as venv:
-        subprocess.run('pytest -vvv --cov=src/{{ package_name }} '
-                       '--cov-report html --cov-report term tests/unit',shell=True, cwd=cwd)    
+        # Install test dependencies
+        print("Installing test dependencies...")
+        venv.install(["pytest", "pytest-cov"])
+        
+        # Run unit tests using venv's python
+        print("Running unit tests...")
+        subprocess.run([
+            venv.python_executable, '-m', 'pytest', '-vvv',
+            '--cov=src/{{ package_name }}', '--cov-report', 'html', '--cov-report', 'term',
+            'tests/unit'
+        ], cwd=cwd)
+        
         if open:
             webbrowser.open_new('file://{}'.format(output_file))
 
@@ -33,8 +43,18 @@ def integration_tests(open=False):
     output_file = os.path.realpath(os.path.join(cwd,'htmlcov','index.html'))
     
     with VirtualEnv(path=venv_path, now=True) as venv:
-        subprocess.run('pytest -vvv --cov=src/{{ package_name }} --cov-report html'
-                       ' --cov-report term tests/integration',shell=True, cwd=cwd)
+        # Install test dependencies
+        print("Installing test dependencies...")
+        venv.install(["pytest", "pytest-cov"])
+        
+        # Run integration tests using venv's python
+        print("Running integration tests...")
+        subprocess.run([
+            venv.python_executable, '-m', 'pytest', '-vvv',
+            '--cov=src/{{ package_name }}', '--cov-report', 'html', '--cov-report', 'term',
+            'tests/integration'
+        ], cwd=cwd)
+        
         if open:
             webbrowser.open_new('file://{}'.format(output_file))
 
@@ -47,8 +67,18 @@ def all_tests(open=False):
     output_file = os.path.realpath(os.path.join(cwd,'htmlcov','index.html'))
     
     with VirtualEnv(path=venv_path, now=True) as venv:
-        subprocess.run('pytest -vvv --cov=src/{{ package_name }} '
-                       '--cov-report html --cov-report term tests/',shell=True, cwd=cwd)    
+        # Install test dependencies
+        print("Installing test dependencies...")
+        venv.install(["pytest", "pytest-cov"])
+        
+        # Run all tests using venv's python
+        print("Running all tests...")
+        subprocess.run([
+            venv.python_executable, '-m', 'pytest', '-vvv',
+            '--cov=src/{{ package_name }}', '--cov-report', 'html', '--cov-report', 'term',
+            'tests/'
+        ], cwd=cwd)
+        
         if open:
             webbrowser.open_new('file://{}'.format(output_file))
 
@@ -60,13 +90,16 @@ def setup_env(open):
     venv_path = os.path.join(cwd, '.venv')
     
     with VirtualEnv(path=venv_path, now=True) as venv:
-        subprocess.run(
-            (
-                "mypy src/{{package_name}} --ignore-missing-imports --html-report typing_report"
-            ),
-            shell=True,
-            cwd=cwd
-        )
+        # Install static analysis dependencies
+        print("Installing static analysis dependencies...")
+        venv.install(["mypy"])
+        
+        # Run mypy using venv's python
+        print("Running static analysis...")
+        subprocess.run([
+            venv.python_executable, '-m', 'mypy', 'src/{{package_name}}',
+            '--ignore-missing-imports', '--html-report', 'typing_report'
+        ], cwd=cwd)
 
         if open:
             webbrowser.open(f'file:://{os.path.join(cwd,"typing_report","index.html")}')
