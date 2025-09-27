b77ea0d6acfe610ce8df5f4f79650ecb4209f543
Dylan Storey <dstorey@gmail.com>
2025-09-27 06:04:54 -0400

    fixed the init as well

--- {{ project_slug }}/.angreal/init.py
+++ {{ project_slug }}/.angreal/init.py
@@ -1,28 +1,38 @@
 import subprocess
+import os
 
 def init():
-    import os
     import angreal
-    
+
     # Get the project directory (one level up from .angreal)
     project_dir = os.path.dirname(angreal.get_root())
     
-    subprocess.run(
-        (
-            "git config --global init.defaultBranch main;"
-            "git init .;"
-            "git add .;"
-            "pip install uv;"
-            "uv venv;"
-            ". .venv/bin/activate;"
-            "uv pip install -e .[dev];"
-            "pre-commit install;"
-            "pre-commit run --all-files;"
-            "git commit -am '{{ project_slug }} initialized via angreal';"
-        ),
-        shell=True,
-        cwd=project_dir
-    )
+    print("Initializing project...")
+    
+    # Set up git
+    subprocess.run(["git", "config", "--global", "init.defaultBranch", "main"], cwd=project_dir, check=True)
+    subprocess.run(["git", "init", "."], cwd=project_dir, check=True)
+    subprocess.run(["git", "add", "."], cwd=project_dir, check=True)
+    
+    # Create and activate virtual environment
+    print("Creating virtual environment...")
+    venv_path = os.path.join(project_dir, ".venv")
+    venv = angreal.VirtualEnv(venv_path)
+    venv.create()
+    venv.activate()
+    
+    # Install dependencies
+    print("Installing dependencies...")
+    venv.install(["-e", ".[dev]"])
+    venv.install(["pre-commit"])
+    
+    # Set up pre-commit
+    print("Setting up pre-commit...")
+    subprocess.run([venv.python_executable, "-m", "pre_commit", "install"], cwd=project_dir, check=True)
+    subprocess.run([venv.python_executable, "-m", "pre_commit", "run", "--all-files"], cwd=project_dir, check=True)
+    
+    # Commit changes
+    print("Creating initial commit...")
+    subprocess.run(["git", "commit", "-am", "test initialized via angreal"], cwd=project_dir, check=True)
 
     print("Initialization complete.")
-    pass
